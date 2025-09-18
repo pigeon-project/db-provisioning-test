5d0f2d39ef5774fc520226da5d5fa5d472510851
pkot <948893+pkot@users.noreply.github.com>
2025-09-18 06:43:41 +0000

    codex version

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -1,33 +1,22 @@
-resource "google_project_service" "compute" {
-  service            = "compute.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "servicenetworking" {
-  service            = "servicenetworking.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "sqladmin" {
-  service            = "sqladmin.googleapis.com"
-  disable_on_destroy = false
-}
-
 resource "google_compute_network" "main" {
-  name                    = var.vpc_name
+  name                    = var.network_name
   auto_create_subnetworks = false
-  depends_on              = [google_project_service.compute]
+
+  depends_on = [
+    google_project_service.compute,
+  ]
 }
 
-resource "google_compute_subnetwork" "primary" {
-  name          = "${var.vpc_name}-primary-${var.region}"
+resource "google_compute_subnetwork" "main" {
+  name          = var.subnet_name
   ip_cidr_range = var.subnet_cidr
-  region        = var.region
   network       = google_compute_network.main.id
+  region        = var.region
 }
 
-resource "google_compute_global_address" "private_service_range" {
-  name          = "${var.vpc_name}-psa-range"
+# Allocate an internal range for Private Service Connect to Cloud SQL (Service Networking)
+resource "google_compute_global_address" "psc_range" {
+  name          = var.psc_range_name
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
@@ -37,11 +26,10 @@ resource "google_compute_global_address" "private_service_range" {
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.main.id
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_service_range.name]
+  reserved_peering_ranges = [google_compute_global_address.psc_range.name]
 
   depends_on = [
     google_project_service.servicenetworking,
-    google_compute_global_address.private_service_range
   ]
 }
 
